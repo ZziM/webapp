a675bb56109aa85564743cf8a2f5af0df1905e55
Denys <zzim4ik@gmail.com>
2025-05-01 17:11:30 +0300

    Init commit

---
+++ webapp.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace webapp.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
